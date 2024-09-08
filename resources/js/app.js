04b230a5a6dc5ff2bd113b1753c14b34547b6a6e
Edy Joel <edyxicon@gmail.com>
2024-09-07 21:34:57 -0600

    Add general layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,33 @@
 import './bootstrap';
 import '../css/app.css';
+import '@mdi/font/css/materialdesignicons.css'
 
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const vuetify = createVuetify({
+    components,
+    directives,
+    icons: {
+        defaultSet: 'mdi',
+    },
+    theme: {
+        themes: {
+            light: {
+                colors: {
+                    primary: '#6875f5',
+                },
+            },
+        },
+    },
+})
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -15,9 +38,10 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(vuetify)
             .mount(el);
     },
     progress: {
-        color: '#4B5563',
+        color: '#B0B0B0',
     },
 });
